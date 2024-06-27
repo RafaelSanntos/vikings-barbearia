2c066a6c83a1fe50ff6f76ea201365d0bb05b893
Rafael Santos <rafaelgamail9@gmail.com>
2024-06-27 10:36:35 -0300

    commit menu agendar agora

--- js/script.js
+++ js/script.js
@@ -1,70 +1,43 @@
-let menu = document.querySelector('#menu-btn');
-let navbar = document.querySelector('.header .navbar');
+$(function(){
+    $("header").load("./components/header.html"); 
+    $("footer").load("./components/footer.html");
+    $("#about").load("./pages/about.html"); 
+    $("#services").load("./pages/services.html");
+    $("#units").load("./pages/units.html");
+    $("#professionals").load("./pages/professionals.html");
+    $("#subscription").load("./pages/subscription.html");
+    $("#banner").load("./pages/banner.html");
+    
+    let swiperReviw = new Swiper(".review-slider", {
+        spaceBetween: 20,
+        grabCursor: true,
+        loop:true,
+        autoplay: {
+            delay: 7500,
+            disableOnInteraction: false,
+        },
+        breakpoints:{
+            0:{
+                slidesPerView:1,
+            },
+            600:{
+                slidesPerView:2,
+            },
+        },
+    });
 
-menu.onclick = () =>{
-    menu.classList.toggle('fa-times');
-    navbar.classList.toggle('active');
-};
-window.onscroll = () =>{
-    menu.classList.remove('fa-times');
-    navbar.classList.remove('active');
-};
-/* Slides */
-
-var swiper = new Swiper(".home-slider", {
-    spaceBetween: 20,
-    effect: "fade",
-    grabCursor: true,
-    loop:true,
-    autoplay: {
-        delay: 4000,  
-    }, 
-    centeredSlides: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-});
-
-var swiper = new Swiper(".review-slider", {
-    spaceBetween: 20,
-    grabCursor: true,
-    loop:true,
-    autoplay: {
-        delay: 7500,
-        disableOnInteraction: false,
-    },
-    breakpoints:{
-        0:{
-            slidesPerView:1,
+    let swiperHome = new Swiper(".home-slider", {
+        spaceBetween: 20,
+        effect: "fade",
+        grabCursor: true,
+        loop:true,
+        autoplay: {
+            delay: 4000,  
+        }, 
+        centeredSlides: true,
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
         },
-        600:{
-            slidesPerView:2,
-        },
-    },
-});
-
-var swiper = new Swiper(".blogs-slider", {
-    spaceBetween: 20,
-    grabCursor: true,
-    loop:true,
-    autoplay: {
-        delay: 7500,
-        disableOnInteraction: false,
-    },
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-    },
-    breakpoints:{
-        0:{
-            slidesPerView:1,
-        },
-        768:{
-            slidesPerView:2,
-        },
-        991:{
-            slidesPerView:3,
-        },
-    },
+    });
 });
